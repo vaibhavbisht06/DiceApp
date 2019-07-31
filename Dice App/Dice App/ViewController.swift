c5f0094bbbd62e952908b7ada8b8e4888031104a
Vaibhav Bisht <vaibhavbisht06@gmail.com>
2019-07-31 22:43:39 +0530

    Linking design with code

--- Dice App/Dice App/ViewController.swift
+++ Dice App/Dice App/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        // Roll Button Code is inside this braces
+    }
+    
 }
 
