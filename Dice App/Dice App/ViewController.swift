86555ec2d6bac25b586aa903c9891e038cc731ef
Vaibhav Bisht <vaibhavbisht06@gmail.com>
2019-08-03 23:25:45 +0530

    Bug Fixed

--- Dice App/Dice App/ViewController.swift
+++ Dice App/Dice App/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     
     var randomDiceIndex2:Int = 0
     
-    let diceArray = [ "dice1","dice2","dice3","dice4","dice5","dice 6" ]
+    let diceArray = [ "dice1","dice2","dice3","dice4","dice5","dice6" ]
     
     @IBOutlet weak var diceImageView1: UIImageView!
     
